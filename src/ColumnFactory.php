f8dc121adf67ec1055db34f7e3ee5203ffecae10
lekraft <development@kv-sys.ru>
2019-01-23 21:04:42 +0500

    Column Factory ready!

--- src/ColumnFactory.php
+++ src/ColumnFactory.php
@@ -1,12 +1,58 @@
 <?php
 namespace Targem\Parser;
 
+/**
+ * Class ColumnFactory.
+ * Factory available column types.
+ * @package Targem\Parser
+ */
 class ColumnFactory {
 
-    public static function create()   {
-        switch (PHP_SAPI) {
+    /**
+     * Login column.
+     * @const string
+     */
+    const LOGIN     = 'Ник';
+
+    /**
+     * Email column.
+     * @const string
+     */
+    const EMAIL     = 'Email';
+
+    /**
+     * Timestamp column.
+     * @const string
+     */
+    const TIMESTAMP = 'Зарегистрирован';
+
+    /**
+     * Status column.
+     * @const string
+     */
+    const STATUS    = 'Статус';
+
+    /**
+     * Creates a database column type object.
+     * @param string $header
+     * @return IColumn
+     */
+    public static function create(string $header) : IColumn   {
+        switch ($header) {
+            case self::LOGIN:
+                $column = new LoginColumn();
+                break;
+            case self::EMAIL:
+                $column = new LoginColumn();
+                break;
+            case self::TIMESTAMP:
+                $column = new LoginColumn();
+                break;
+            case self::STATUS:
+                $column = new LoginColumn();
+                break;
             default:
-                $column = new TimestampColumn();
+                $column = new DefaultColumn();
         }
         return $column;
     }
