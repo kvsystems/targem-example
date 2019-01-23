d4c5b659b623df3f2f49dded4c93fb76e40d97fb
lekraft <development@kv-sys.ru>
2019-01-23 22:12:27 +0500

    Abstract Database created

---
+++ src/database/ADatabase.php
@@ -0,0 +1,57 @@
+<?php
+namespace Targem\Parser\Database;
+
+use Targem\Parser\IColumn;
+
+/**
+ * Abstract class ADatabase.
+ * @package Targem\Parser\Database
+ */
+abstract class ADatabase {
+
+    /**
+     * Columns list.
+     * @var $columns array
+     */
+    protected $columns = [];
+
+    /**
+     * Content list.
+     * @var $data array
+     */
+    protected $data = [];
+
+    /**
+     * Creates new table.
+     * @param string $name
+     * @return bool
+     */
+    public abstract function table(string $name) : bool;
+
+    /**
+     * Inserts data in new table.
+     * @param string $table
+     * @return bool
+     */
+    public abstract function insert(string $table) : bool;
+
+    /**
+     * Sets a new column.
+     * @param IColumn $column
+     * @return bool
+     */
+    public function column(IColumn $column) : bool {
+
+        return true;
+    }
+
+    /**
+     * Sets new csv row.
+     * @param $row array
+     * @return bool
+     */
+    public function data(array $row) : bool {
+
+        return true;
+    }
+}
