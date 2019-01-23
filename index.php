6c19938a28932f5d773b7d759ce1d18922ce4ef9
lekraft <development@kv-sys.ru>
2019-01-24 00:17:40 +0500

    client code updated

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 namespace Targem\Parser;
 
 use Targem\Parser\Database\MysqlDatabase as Mysql;
+use Targem\Parser\Column\ColumnFactory as Factory;
 
 /** PHP configuration */
 ini_set( 'error_reporting', E_ALL );
@@ -12,22 +13,30 @@ date_default_timezone_set( 'Asia/Yekaterinburg' );
 /** Composer psr-4 autoload **/
 require_once __DIR__ . '/vendor/autoload.php';
 
+/** Trying main script execution **/
 try {
 
     $parser = new CsvParser(__DIR__ . '/upload/gamers.csv', true);
-
-    $headers = $parser->headers();
-    $contents = $parser->content();
-
-    for($i = 0; $i < count($headers); $i++)  {
-
-    }
-
     $database = new Mysql('', '', '', '');
 
-    //$database->table();
+    $content = $parser->content();
 
-    //ColumnFactory::create();
+    foreach($parser->headers() as $key => $header)  {
+        $obj = Factory::create($header);
+        for($i = 0; $i < count($content); $i++) {
+            $content[$i][$key] = $obj->transform($content[$i][$key]);
+        }
+        $database->column($obj);
+    }
+
+    $database->data($content);
+
+    if(!$database->table('trg_gamers'))
+        throw new \Exception('Could not create table');
+
+    if(!$database->insert('trg_gamers'))
+        throw new \Exception('Could not insert into table');
+
 } catch (\Exception $e) {
     echo $e->getMessage() . PHP_EOL;
 }
